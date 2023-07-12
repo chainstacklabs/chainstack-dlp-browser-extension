fb00f5c94f2c1486a45571f895ccd2b3e5f0971e
Soos3D <99700157+soos3d@users.noreply.github.com>
2023-07-12 16:31:08 -0300

    refactor for manifest V3

--- background/background.js
+++ background/background.js
@@ -17,3 +17,19 @@ chrome.runtime.onInstalled.addListener(function () {
     },
   });
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        files: ["dist/bundle.js"],
+      })
+      .then(() => {
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          files: ["content/content.js"],
+        });
+      });
+  }
+});
--- content/content.js
+++ content/content.js
@@ -1,72 +1,79 @@
-window.addEventListener(
-  "message",
-  console.log("message"),
-  function (event) {
-    // We only accept messages from ourselves
-    if (event.source != window) return;
+// content.js
 
-    if (event.data.type && event.data.type == "DATA_REDACTOR") {
-      console.log("Content script received: " + event.data.text);
+let hasUpdated = false; // <-- Add a flag here
+
+function updateInputBox() {
+  // Initialize dataRedactor with the current settings
+  chrome.storage.sync.get("redactSettings", function (items) {
+    let dataRedactor = new window.data_redactor(items.redactSettings);
+    console.log("Settings:", items.redactSettings);
+    console.log("Test text:", dataRedactor.redact("My name is David"));
+
+    let oldInputBox = document.querySelector("#prompt-textarea");
+    if (!oldInputBox) {
+      console.error("Old input box not found");
+      return;
     }
-  },
-  false
-);
 
-// Create a Promise that resolves when the bundle.js script is loaded
-let bundlePromise = new Promise((resolve) => {
-  fetch(chrome.runtime.getURL("dist/bundle.js"))
-    .then((r) => r.text())
-    .then((text) => {
-      let script = document.createElement("script");
-      script.textContent = text;
-      (document.head || document.documentElement).appendChild(script);
-
-      console.log("Script injected:", script);
-
-      resolve(); // Resolve the Promise
-      console.log("this", Window.data_redactor);
-    });
-});
-
-bundlePromise.then(() => {
-  // The bundle.js script has been loaded
-  // Now load the settings and setup the input box
-  chrome.storage.sync.get(["enabled", "redactSettings"], function (items) {
-    console.log("Settings loaded:", items); // Log the loaded settings
-    if (items.enabled) {
-      console.log("window:", window); // Check if window.data_redactor is defined
-      console.log("window.data_redactor:", window.data_redactor); // Check if window.data_redactor is defined
-      let dataRedactor = window.data_redactor; // Try to capture window.data_redactor in a closure
-
-      // Create the new input box
-      let newInputBox = document.createElement("textarea");
+    // Insert the new input box if it doesn't exist yet
+    let newInputBox = document.querySelector("#newInputBox");
+    let tokenCountElement = document.querySelector("#tokenCount"); // <-- Get the token count element
+    if (!newInputBox) {
+      newInputBox = document.createElement("textarea");
       newInputBox.id = "newInputBox";
       newInputBox.style.color = "black";
 
-      // Find the existing input box
-      let oldInputBox = document.querySelector("#prompt-textarea");
-      if (!oldInputBox) {
-        console.error("Old input box not found");
-        return;
-      }
+      // Create a token count element
+      tokenCountElement = document.createElement("div");
+      tokenCountElement.id = "tokenCount";
+      tokenCountElement.style.color = "white";
 
-      // Insert the new input box above the old one
+      // Insert the new elements
       oldInputBox.parentNode.insertBefore(newInputBox, oldInputBox);
+      oldInputBox.parentNode.insertBefore(tokenCountElement, newInputBox); // <-- Insert it before the new input box
+    }
 
-      // When the new input box loses focus, redact the text and put it in the old input box
-      newInputBox.onblur = function () {
-        console.log("New input box blurred"); // Log when the input box loses focus
+    // When the new input box loses focus, redact the text and put it in the old input box
+    newInputBox.onblur = function () {
+      let text = newInputBox.value;
+      let redactedText = dataRedactor.redact(text);
+      let token_count = dataRedactor.countTokens(redactedText);
+      console.log("Redacted text:", redactedText);
+      console.log("Tokens count:", token_count);
 
-        // Get the text from the new input box
-        let text = newInputBox.value;
-        console.log(`text passed: ${text}`);
-        console.log(dataRedactor);
-        // Redact the text
-        let redactedText = dataRedactor.redact(text); // Use captured dataRedactor
+      // Set the value and dispatch an input event to notify any listeners that the value has changed
+      oldInputBox.value = redactedText;
+      oldInputBox.dispatchEvent(new Event("input", { bubbles: true }));
 
-        // Put the redacted text in the old input box
-        oldInputBox.value = redactedText;
-      };
+      // Update the token count element
+      tokenCountElement.textContent = "Token count: " + token_count;
+
+      // Clear the new input box
+      newInputBox.value = "";
+    };
+  });
+}
+
+// Create a MutationObserver to watch for changes in the DOM
+let observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    // If the addedNodes property has one or more nodes
+    if (!hasUpdated && mutation.addedNodes.length) {
+      // <-- Check the flag here
+      let oldInputBox = document.querySelector("#prompt-textarea");
+      if (oldInputBox) {
+        // The input box has been added, update it
+        updateInputBox();
+        // Stop observing changes to the DOM
+        observer.disconnect();
+        hasUpdated = true; // <-- Set the flag here
+      }
     }
   });
+});
+
+// Start observing changes to the DOM
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
 });
